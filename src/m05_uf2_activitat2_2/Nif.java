bdd1b8e5d5673658e9d8cbb0f32caac50567e6df
ALUMNEDAM <ALUMNEDAM@192.168.120.112>
2016-11-03 21:13:23 +0100

    Dote hermoso

--- src/m05_uf2_activitat2_2/Nif.java
+++ src/m05_uf2_activitat2_2/Nif.java
@@ -4,40 +4,50 @@ package m05_uf2_activitat2_2;
 public class Nif {
     
     private String nif;
+    private String missatge;
+    private char[] llista = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
     
     public void esValid() {
 
-        char[] llista = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
-
+        
         int num = 0;
         String letra;
         String numero;
-        int longitudCadena1=8;
-        int longitudCadena2=9;
 
         letra = nif.substring(nif.length() - 1);
         numero = nif.substring(0, nif.length() - 1);
 
         // primer mirem que la cadena tingui 8 o 9 caràcters:
 
-        if (nif.length() < longitudCadena1 || nif.length() > longitudCadena2) {
-            System.err.println("Format Incorrecte");
+        if (longitudCadena(nif.length())) {
+            missatge = "Format Incorrecte";
         } // Després mirem que el número sigui convertible a enter.
-        else {
+         else {
             try {
                 num = Integer.parseInt(numero);
 
-                if (String.valueOf(llista[num % 23]).equals(letra)) {
-                    System.err.println("Nif Vàlid");
+                if (comprobarLetraDni()) {
+                    missatge =  "Nif Vàlid";
                 } else {
-                    System.err.println("Nif Invàlid");
+                    missatge = "Nif Invàlid";
                 }
-            } catch (NumberFormatException e) {
-                System.err.println("Format Incorrecte");
-            }
 
-        }
+            } catch (NumberFormatException e) {
+                missatge = "Format Incorrecte";
+            }
     
+        }
     }
+    public boolean longitudCadena(int nif){
+        int longitudCadena1=8;
+        int longitudCadena2=9;
+        return (nif < longitudCadena1 || nif > longitudCadena2);
+    }
+    
+    public boolean comprobarLetraDni (int num, String letra){
+        
+        return (String.valueOf(llista[num % 23]).equals(letra));
+    }
+
 }
